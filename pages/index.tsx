e326b4a3449557ec1a47000fd1ae31aa5169a96d
Max Malm <benjick@dumfan.net>
2022-04-16 11:34:48 +0200

    make image clickable

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,49 +10,46 @@ const Home: NextPage<Props> = ({ recipes }) => {
   return (
     <div>
       <main>
-        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="relative px-4 pt-16 pb-20 bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
           <div className="absolute inset-0">
             <div className="bg-white h-1/3 sm:h-2/3" />
           </div>
-          <div className="relative max-w-7xl mx-auto">
+          <div className="relative mx-auto max-w-7xl">
             <div className="text-center">
-              <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+              <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                 Recipes
               </h2>
-              <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+              <p className="max-w-2xl mx-auto mt-3 text-xl text-gray-500 sm:mt-4">
                 Tiny collection of stuff I cook all the time.
               </p>
             </div>
-            <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+            <div className="grid max-w-lg gap-5 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none">
               {recipes.map((recipe) => (
-                <div
-                  key={recipe.slug}
-                  className="flex flex-col rounded-lg shadow-lg overflow-hidden"
-                >
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-48 w-full object-cover"
-                      src={recipe.image}
-                      alt=""
-                    />
-                  </div>
-                  <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                    <div className="flex-1">
-                      <a href={recipe.slug} className="block mt-2">
+                <a key={recipe.slug} href={recipe.slug} className="block mt-2">
+                  <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="object-cover w-full h-48"
+                        src={recipe.image}
+                        alt=""
+                      />
+                    </div>
+                    <div className="flex flex-col justify-between flex-1 p-6 bg-white">
+                      <div className="flex-1">
                         <p className="text-xl font-semibold text-gray-900">
                           {recipe.name}
                         </p>
-                      </a>
-                    </div>
-                    <div className="mt-6 flex items-center">
-                      <div className="flex space-x-1 text-sm text-gray-500">
-                        <span>Serves {recipe.serves} people</span>
-                        <span aria-hidden="true">&middot;</span>
-                        <span>{recipe.duration} cooking time</span>
+                      </div>
+                      <div className="flex items-center mt-6">
+                        <div className="flex space-x-1 text-sm text-gray-500">
+                          <span>Serves {recipe.serves} people</span>
+                          <span aria-hidden="true">&middot;</span>
+                          <span>{recipe.duration} cooking time</span>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
+                </a>
               ))}
             </div>
           </div>
