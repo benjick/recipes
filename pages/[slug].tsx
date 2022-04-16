6f750d919de0303a834de7f99338d4945fd4e17e
Max Malm <benjick@dumfan.net>
2022-04-16 11:36:23 +0200

    fix ingredient labels

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -13,18 +13,19 @@ interface Props {
 }
 
 const CheckItem: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const id = `id-${children}`;
   return (
     <div className="relative flex items-start">
       <div className="flex items-center h-5">
         <input
-          id="comments"
+          id={id}
           name="comments"
           type="checkbox"
           className="w-6 h-6 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
         />
       </div>
       <div className="ml-3 text">
-        <label htmlFor="comments" className="font-medium text-gray-700">
+        <label htmlFor={id} className="font-medium text-gray-700">
           {children}
         </label>
       </div>
